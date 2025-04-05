e665d47a4cdc01875258a175eddb645cdefc94a9
Livnat Arama <livnatar@std.hac.ac.il>
2025-04-05 21:14:44 +0300

    add comment on GameManager.h for future

--- Chess/include/GameManager.h
+++ Chess/include/GameManager.h
@@ -19,6 +19,9 @@ public:
 private:
     std::unique_ptr<ChessBoard> m_chessBoard;
     bool m_isWhiteTurn; // true = White's turn, false = Black's turn
+    
+    // TODO (future): Add vectors to store captured pieces 
+    // for use in UI display, undo functionality, or game history tracking
 
    
     std::pair<int, int> convertPosition(const std::string& pos) const;
